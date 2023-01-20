47ee241697ea6bdacb53ab24036e2c4d3c2c2559
kiran kumar <ch.kirankumar311.com>
2023-01-20 22:24:43 +0530

    nav-setup

---
+++ src/components/firebase.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB2eyMmwAK0TauT2LnU61CGMyN1Nn0YKQk",
+    authDomain: "disney-clone-2ed8a.firebaseapp.com",
+    projectId: "disney-clone-2ed8a",
+    storageBucket: "disney-clone-2ed8a.appspot.com",
+    messagingSenderId: "785912489387",
+    appId: "1:785912489387:web:98474076b8331d7ada1eee",
+    measurementId: "G-KJFC578SLZ"
+  };
+
+  // Initializing the firebase
+  const firebaseApp= firebase.initializeApp(firebaseConfig);
+
+  //conecting with data base
+
+  

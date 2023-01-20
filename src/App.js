a1c6e00164cafefccc07356a7b28952ed5aa59e3
kiran kumar <ch.kirankumar311.com>
2023-01-20 18:47:31 +0530

    login-background-image

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+      <Route path='/' element={<Login/>}/>
+      </Routes>
+ 
+    </div>
+  );
+}
+
+export default App;
